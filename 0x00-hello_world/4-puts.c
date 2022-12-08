edbfd24835b2bd22144849e59748e241e57b9118
prex12 <allihuzzain@gmail.com>
2022-12-08 09:22:26 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char word[]= "\"Programming is like building a multilingual puzzle";
+	char word[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(word);
 	return(0);
