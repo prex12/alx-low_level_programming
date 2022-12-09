c99293e313f04bbf02030762b26175cd4d473d65
prex12 <allihuzzain@gmail.com>
2022-12-09 13:29:04 -0800

    first

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
  * main - Entry point
  *
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -15,5 +15,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
- 
 }
