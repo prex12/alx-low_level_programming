2d15cb271c91215e1b0de627832eda449f071439
prex12 <allihuzzain@gmail.com>
2022-12-09 10:49:59 -0800

    numbers below 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int a;
+	a=0;
+	while (a < 0)
+		printf("%d\n", a);
+		a++;
+	return (0);
+}
