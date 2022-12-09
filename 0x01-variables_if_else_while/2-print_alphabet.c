87a594bb8bbd5288bdef448642a9c834e9b8e901
prex12 <allihuzzain@gmail.com>
2022-12-09 09:36:51 -0800

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char lttr
+		;
+	for (lttr = 'a'; lttr <= 'z'; lttr++)
+	{
+		putchar(lttr);
+	}
+	putchar('\n');	
+	return (0);
+ 
+}
