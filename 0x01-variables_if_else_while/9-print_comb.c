fb2256b0d79174537f2a625dd8d48fd716aa20b1
prex12 <allihuzzain@gmail.com>
2022-12-10 15:04:34 -0800

    combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
