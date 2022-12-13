a3ead361bdce4870751df1fef610f3b6bde25860
prex12 <allihuzzain@gmail.com>
2022-12-13 14:15:27 -0800

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds a nd b
+ * @a: integer to be added to b
+ * @b: integer to be added to a
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	int ans;
+
+	ans = a + b;
+	return (ans);
+}
