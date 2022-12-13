5749f09e39e9b4b6f0ee10681f2f7ab4c8518fe0
prex12 <allihuzzain@gmail.com>
2022-12-13 05:20:57 -0800

    alphabet 10x

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,3 +20,20 @@ void print_alphabet(void)
 		_putchar(a);
 	_putchar('\n');
 }
+void print_alphabet_x10(void)
+{
+	int b = 0;
+	char a;
+
+	while (b <= 10)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
