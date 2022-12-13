c314acbf6caf3206665b6a62d71571473f1d0a3a
prex12 <allihuzzain@gmail.com>
2022-12-13 15:04:47 -0800

    x10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - prints all lowercase alphabets
- * 
+ *
  * followed by new line
  */
 void print_alphabet(void)
 {
 	char a;
+
 	for (a = 'a'; a <= 'z'; a++)
 		_putchar(a);
 	_putchar('\n');
