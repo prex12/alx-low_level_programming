aa29c8d46668e6502910c4db0e99bf00de812144
prex12 <allihuzzain@gmail.com>
2022-12-13 14:43:49 -0800

    are they alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _isalpha -  checks for alphabetic character
+ * @c: int to be checked
+ * Return: print 1 if c is lowercase or uppercase
+ * print 0 otherwise
+ */
 
 int _isalpha(int c)
 {
