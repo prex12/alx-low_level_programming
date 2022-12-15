7ba06cc27ce4ae4dadfd5cff5a6ea2e2dda27fb7
prex12 <allihuzzain@gmail.com>
2022-12-15 04:34:24 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all single digit numbers but 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50 && a != 52)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 10 times followed by new line
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a <= 15; a++)
+		{
+			if  (a >= 10)
+			{
+				_putchar(a / 10 + '0');
+			}
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
