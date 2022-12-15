6ddeb298a91e7a7f43f7ff10474eb740e457ae6c
prex12 <allihuzzain@gmail.com>
2022-12-15 05:48:18 -0800

    lines are here

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
+/**
+ * print_line - prints n number of line
+ * followed by next line
+ * @n: the int that controls the length of the line
+ */
 void print_line(int n)
 {
+	int a;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		int a;
-
 		for (a = 0; a <= n; a++)
 			_putchar('_')
 		_putchar('\n');
