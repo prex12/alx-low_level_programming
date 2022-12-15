cfcdd9468a73d3274c5e7444040f43b376c478d5
prex12 <allihuzzain@gmail.com>
2022-12-15 05:30:37 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,13 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (b = 0; b < 10; b++)
+	for (a = 0; a < 10; a++)
 	{
-		for (a = 0; a <= 15; a++)
+		for (b = 0; b <= 15; b++)
 		{
-			if  (a >= 10)
-			{
-				_putchar(a / 10 + '0');
-			}
-			_putchar(a % 10 + '0');
+			if  (b >= 10)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
