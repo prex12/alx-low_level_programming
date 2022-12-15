dcd154ad5dff8110cc0ee64c76939bab77c01985
prex12 <allihuzzain@gmail.com>
2022-12-15 01:22:50 -0800

    multiply them

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies the values of a and b
+ *
+ * @a: int to be multiplied with b
+ * @b: int to be multiplied with a
+ *
+ * Return: result to the main function
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
