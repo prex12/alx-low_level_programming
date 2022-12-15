de5585e7afa365341a52dce6899396c5e0e91913
prex12 <allihuzzain@gmail.com>
2022-12-15 01:03:11 -0800

    is it uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for UPPERCASE characters in int c
+ *
+ * @c: int to be checked
+ * Return: 1 if c is an UPPERCASE alphabet and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
