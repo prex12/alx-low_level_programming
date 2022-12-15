4ab94e628c459ed2a91fb273c3e5118038315f5c
prex12 <allihuzzain@gmail.com>
2022-12-15 13:44:38 -0800

    diagonal prints

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,9 +15,9 @@ void print_diagonal(int n)
 	{
 		int a, b;
 
-		for (a = 0; a <= n; a++)
+		for (a = 0; a < n; a++)
 		{
-			for (b = 0; b <= a; b++)
+			for (b = 0; b < a; b++)
 			{
 				if (b == a)
 					_putchar(98);
