cd4341173646f2755612ddbcda34d12346c6662b
prex12 <allihuzzain@gmail.com>
2022-12-15 14:43:38 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if ((a % 3) == 0 && a % 5 != 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if ((a % 5) == 0 && a % 3 != 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else if ((a % 3) == 0 && (a % 5) == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else if (a == 1)
 			printf("%d", a);
 		else
