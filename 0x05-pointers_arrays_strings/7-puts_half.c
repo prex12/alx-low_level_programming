0471adc8d1404f1a87055837fe4d61e4e93b4e06
prex12 <allihuzzain@gmail.com>
2022-12-19 15:25:32 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,18 @@ void puts_half(char *str)
 	int a, b, c;
 
 	a = 0;
-	 while (str[a] != '\0')
+
+	while (str[a] != '\0')
 		a++;
-	 if (a % 2 == 0)
-	 {
-	 	for (b = a / 2; str[b] != '\0'; b++)
+	if (a % 2 == 0)
+	{
+		for (b = a / 2; str[b] != '\0'; b++)
 			_putchar(str[b]);
-	 }
-	 else
-	 {
-	 	for (c = (a - 1) / 2; c < a - 1; c++)
+	}
+	else if (a % 2)
+	{
+		for (c = (a - 1) / 2; c < a - 1; c++)
 			_putchar(str[c - 1]);
-	 }
-	 _putchar('\n');
+	}
+	_putchar('\n');
 }
