fa2fc968fa1359164fa5a4dbc842a203e2843ac3
prex12 <allihuzzain@gmail.com>
2022-12-19 09:02:56 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string in reverse
+ *@s: pointer to the string input
+ */
+
+void print_rev(char *s)
+{
+	int a, b, c;
+
+	a = 0;
+
+	while (s[a] != '\0')
+		a++;
+	b = a;
+
+	for (c = b - 1; c >= 0; c--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
