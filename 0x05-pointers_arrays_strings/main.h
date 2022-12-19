777abc4bc8ae6eeea46783b1bb94091f79abe6ff
prex12 <allihuzzain@gmail.com>
2022-12-19 14:11:38 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void rev_string(char *c);
 void print_rev(char *s);
 void prit_array(int *a, int n);
