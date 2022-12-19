dc8fb7940c4372d18205a62fb7d57d41a62c474c
prex12 <allihuzzain@gmail.com>
2022-12-19 07:35:36 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - computes the length of string
+ *
+ * @s: variable holding the string value
+ * Return: q
+ */
+
+int _strlen(char *s)
+{
+	int q;
+
+	for (q = 0; s[q] != '\0'; q++)
+
+	return (q);
+}
