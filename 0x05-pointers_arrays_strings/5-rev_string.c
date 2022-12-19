0242cb40b5347c856fb4b5a89eb41dfd77826185
prex12 <allihuzzain@gmail.com>
2022-12-19 14:09:42 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	int a, b, c;
+	char me;
+
+	a = 0;
+	b = 0;
+
+	while (s[a] != '\0')
+		a++;
+
+	b = a - 1;
+
+	for (c = 0; c < a / 2;  c++)
+	{
+		sm = s[c];
+		s[c] = s[b];
+		s[b--] = c;
+	}
+}
