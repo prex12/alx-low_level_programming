c480a679f240846ff19be2e555abf846f07618e4
prex12 <allihuzzain@gmail.com>
2022-12-22 00:46:45 -0800

    task 0 added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main. h"
+
+/**
+ * _strcat - concatinates a source string to a destination string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: destination string with appended string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	for (b = 0; src[b] != '\0'; b++)
+		dest[a + b] = src[b];
+	dest[a] = '\0';
+
+	return (dest);
+}
