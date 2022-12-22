e2b27bf409351a205593a8661c2789933e91a340
prex12 <allihuzzain@gmail.com>
2022-12-22 00:56:34 -0800

    task 0 added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,7 +18,10 @@ char *_strcat(char *dest, char *src)
 	while (dest[a] != '\0')
 		a++;
 	for (b = 0; src[b] != '\0'; b++)
-		dest[a + b] = src[b];
+	{
+		dest[a] = src[b];
+		a++;
+	}
 	dest[a] = '\0';
 
 	return (dest);
