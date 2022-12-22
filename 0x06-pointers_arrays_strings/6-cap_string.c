2ab15b275d8ff2bfef65456f0dcd7fa5da26c020
prex12 <allihuzzain@gmail.com>
2022-12-22 12:01:11 -0800

    task 6 added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - transforms first letter
+ * of every word to uppercase
+ * @str: pointer to each character in string
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int a;
+	char me[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	a = 0;
+	while (str[a] != '\0')
+		if (a == 0 && str[a] >= 'a' && str[a] <= 'z')
+			str[a] = str[a] - 32;
+		for (int b = 0; b < 13; b++)
+		{
+			if (str[a] == me[b])
+			{
+				if (str[a + 1] >= 'a' && str[a + 1] <= 'z')
+					str[a + 1] = str[a + 1] - 32;
+			}
+		}
+		a++;
+	return (str);
+}
