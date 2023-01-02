5ab254c01c676b36fbcdc1b9ccf62198e3a02fc4
prex12 <allihuzzain@gmail.com>
2023-01-02 01:11:25 -0800

    task 0 added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset - changes the string s to b in memory guided by n bytes
+ * @s: pointer to a string variable
+ * @b: memory change parameter
+ * @n: number of bytes
+ * Return: pointer value of s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int a = 0; a < n; a++)
+		s[a] = b;
+	return (s);
+}
