fa703f0f55f0e614183b16c98a8bec42841276aa
prex12 <allihuzzain@gmail.com>
2023-01-02 09:29:38 -0800

    task 6 added

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,9 +8,9 @@ void print_chessboard(char (*a)[8])
 {
 	int c, b;
 
-	for (c = 0; c < '\0'; c++)
+	for (c = 0; c < 8; c++)
 	{
-		for (b = 0; b != '\0'; b++)
+		for (b = 0; b != 8; b++)
 		{
 			_putchar(a[c][b]);
 		}
