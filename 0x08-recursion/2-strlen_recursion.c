683a7e23f6d7bfd2178221d9b0029f0b185b9584
prex12 <allihuzzain@gmail.com>
2023-01-04 05:18:56 -0800

    task 2 added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - counts the number of characters
+ * in a string
+ * @s: pointer to the character
+ * Return: number of character
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s == '\0')
+		return (a);
+	a++;
+	_strlen_recursion(s + 1);
+}
