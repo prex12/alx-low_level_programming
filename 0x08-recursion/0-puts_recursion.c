8b48da41b4f256402d02fab962ff530e391a65ac
prex12 <allihuzzain@gmail.com>
2023-01-04 04:34:53 -0800

    task 0 added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: points to each char in a string
+ * Return: s when the NULL terminator is reached
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return (s);
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
