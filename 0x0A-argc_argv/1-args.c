4845a34ac17235dbe4e5bc35053c609849d25f48
prex12 <allihuzzain@gmail.com>
2023-01-06 13:05:57 -0800

    task 1 added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguements
+ * @argc: stores the number of arguements
+ * @argv: array of chars
+ * Return: 0 success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
