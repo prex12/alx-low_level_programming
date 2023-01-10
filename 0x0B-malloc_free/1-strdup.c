a11a353d5399506e3116550a27324f24d0fbc4ca
prex12 <allihuzzain@gmail.com>
2023-01-10 00:09:27 -0800

    task one added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	b = 0;
 	while (str[b])
 		b++;
-	new = (char *) malloc((n + 1) * sizeof(char));
+	new = malloc((b + 1) * sizeof(char));
 
 	if (new == NULL)
 		return (NULL);
