ad353f1532a433c859dd1dca07d2e4ffac4fbbfe
prex12 <allihuzzain@gmail.com>
2023-01-09 22:31:20 -0800

    task 1 added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - allocates space in heap memory for
+ * @size: size of array
+ * @c: character used to initialize array
+ * Return: NULL on error and pointer to the array on
+ * success
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n;
+	char *a;
+
+	a = (char *) malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	if (a == NULL)
+		return (0);
+	for (n = 0; n < size; n++)
+	{
+		*(a + n) = c;
+	}
+	*(a + n) = '\0';
+
+	return (a);
+}
