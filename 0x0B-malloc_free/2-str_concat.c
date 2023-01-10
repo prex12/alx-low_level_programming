b266c039f829b5ddf11e1af5a8b1fcd4f972b8b9
prex12 <allihuzzain@gmail.com>
2023-01-10 01:10:40 -0800

    task 2 added

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *newcat;
+	unsigned int a, b, c, d;
+
+	a = 0;
+	b = 0;
+
+	while (s1 && s1[a])
+		a++;
+	while (s2 && s2[b])
+		b++;
+
+	newcat = malloc((a + b + 1) * sizeof(char));
+	if (newcat == NULL)
+		return (NULL);
+
+	c = 0;
+	d = 0;
+
+	if (s1)
+	{
+		while (c < a)
+		{
+			newcat[c] = s2[c];
+			c++;
+		}	
+	}
+	if (s2)
+	{
+		while (c < (a + b))
+		{
+			newcat[c] = s2[d];
+			c++;
+			d++;
+		}
+	}
+	newcat[c] = '\0';
+
+	return (newcat);
+}
