d0f73c9933866eb10103ef3300fdd8c268c12fbb
prex12 <allihuzzain@gmail.com>
2023-01-09 23:45:35 -0800

    task 1 added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - copies a given string passedas arg
+ * @str: pointer to the string duplicate
+ * Return: NULL if error is encountered
+ * and pointer to deplicate string
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	int n, b;
+
+	if (str == NULL)
+		return (NULL);
+	b = 0;
+	while (str[b])
+		b++;
+	new = ((n + 1) * sizeof(char));
+
+	if (new == NULL)
+		return (NULL);
+	while ((new[n] = str[n]) != '\0')
+		n++;
+
+	return (new);
+}
