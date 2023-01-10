225bdc38a1286791f93ea8fb9e20bc5550f23907
prex12 <allihuzzain@gmail.com>
2023-01-10 03:26:30 -0800

    task 3 added

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	c = (int **) malloc(sizeof(int*) * height);
-	if (c = NULL)
+	if (c == NULL)
 		return (NULL);
 
 	for (a = 0; a < height; a++)
