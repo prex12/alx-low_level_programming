e6b9a6b9153cc3dbf41757057737b6251cdfac36
prex12 <allihuzzain@gmail.com>
2023-01-16 04:30:52 -0800

    task 3 added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,10 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef new name for type struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
