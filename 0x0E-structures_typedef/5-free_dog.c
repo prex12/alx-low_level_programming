0cd41ff51479960d7cae615487e15e5756723483
prex12 <allihuzzain@gmail.com>
2023-01-16 04:48:25 -0800

    task 5 added

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- *
+ * free_dog - frees heap memory
+ * @d: pointer to d
  */
 void free_dog(dog_t *d)
 {
-	free(d);
 	free(d->name);
 	free(d->owner);
+	free(d);
 }
